ea08f9eb355ddeefe37466effaba877070daa8df
Bogdan Ganea <bogdanionutganea@gmail.com>
2022-11-24 10:53:52 +0200

    24nov2022

---
+++ p3231.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n,i;
+for (i=1;i>=n;i--)
+    cout<<n<<endl;
+for (i=1;i<=n;i++)
+    cout<<n;
+}
+
---
+++ p3983.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cin>>n>>m;
+    for(int i=n;i>=1;i--)
+        cout<<m*i<<" ";
+return 0;
+   }
