266875bc6f247bc5139da96f62c5534ff501430b
apartisan <another.partisan@yahoo.com>
2023-03-22 12:24:05 +0200

    test

--- test tablouri.cpp
+++ test tablouri.cpp
@@ -2,34 +2,13 @@
 using namespace std;
 int main (){
     int v[7]= {1,1,2,0,4,5,6};
-    int i, n=7;
-
-    i=0;
-    while (i<n){
-        cout<<v[i];
-        i++;
-    }
-    cout<<endl;
-
-    i=0;
-    while (i<n){
-        i++;
-        cout<<v[i];
-    }
- cout<<endl;
-
-    i=0;
+    int i=0, nr=0;
     do{
-        i++;
-        cout<<v[i];
-    }while (i<n);
+        if (i==v[i])
+            nr++;
+    }while(i<6 && v[i++]);
+    cout<<nr;
 
- cout<<endl;
-    i=0;
-    do{
-        cout<<v[i];
-        i++;
-    }while (i<n);
 
 
 }
